083b7a021a13fba55cbb8798bca7ec0b2817b4b4
EduardoHaibleForti <eduardo.hf@puccampinas.edu.br>
2024-09-06 11:54:44 -0300

    Add files via upload

---
+++ listateorica2b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    char nomeUsuario[50];
+    printf("Ola eu sou o Numero 1, qual o seu nome :\n");
+    scanf("%s",&nomeUsuario);
+
+    printf("Bem vindo ao clube :%s\n",nomeUsuario);
+
+
+
+  return 0;
+}
